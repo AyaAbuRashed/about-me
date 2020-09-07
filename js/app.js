9945689b89bcecc3ee5f2582a63e413662f87fff
AyaAbuRashed <ayanaser402@gmail.com>
2020-09-07 18:18:36 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+var name =prompt("write your name");
+console.log("welcome "+name );
+alert("welcome"+name);
+var a=prompt("Is Html stands for hypertext markup language?yes/no");
+a=a.toLowerCase();
+if(a =="yes")
+{alert("Correct answer");}
+else if (a=="no")
+{
+    alert("InCorrect answer");
+}
+var b=prompt("Is CSS stand for Cascading Style Sheets?yes/no");
+b=b.toLowerCase();
+if(b=="yes")
+{alert("Correct answer");}
+else if (b=="no")
+{
+    alert("InCorrect answer");
+}
+var c=prompt("Is  JavaScript a programming language?yes/no");
+c=c.toLowerCase();
+if(c =="yes")
+{alert("Correct answer");}
+else if (c=="no")
+{
+    alert("InCorrect answer");
+}
+var x=prompt("Is JavaScript written in CSS?yes/no");
+x=x.toLowerCase();
+if(x =="yes")
+{alert("Correct answer");}
+else if (x=="no")
+{
+    alert("InCorrect answer");
+}
+var y=prompt("Is the Head tag means content ?yes/no");
+y=y.toLowerCase();
+
+if(y =="yes")
+{alert("Correct answer"+name);}
+else if (y=="no")
+{
+    alert("InCorrect answer"+name);
+}
